7200d4020899f3cfedae04efc8bce0e4c23ee4cb
dwinzg <75581482+dwinzg@users.noreply.github.com>
2024-04-10 19:34:34 -0700

    edits to ipynb and two function files

--- src/relevant_features.py
+++ src/relevant_features.py
@@ -4,6 +4,19 @@
 import pandas as pd
 import numpy as np
 def relevant_features(correlation_matrix, target_variable):
+    '''
+    Takes a correlation matrix and a target variable and returns a list of relevant features.
+
+    Parameters:
+    ----------
+    correlation_matrix (pandas dataframe): Correlation matrix.
+    target_variable (str): Target variable.
+
+    Returns:
+    -------
+    relevant_features (list): List of relevant features.
+
+    '''
     # Check for empty inputs
     if target_variable not in correlation_matrix.columns:
         return None
